6f126c715aff1c648709b7b9a0ed20c3a5717836
Enkhbold470 <enkhbold470@gmail.com>
2024-08-18 11:50:30 -0700

    NEXT_PUBLIC is important, required for app directory

--- src/components/home/postBox.tsx
+++ src/components/home/postBox.tsx
@@ -4,28 +4,17 @@ import useScrollSnap from "react-use-scroll-snap";
 import { PostBoxSkeleton } from "./postSkeleton";
 import { Suspense } from "react";
 import { fetchPhotos, getCuratedPhotos } from "@/lib/pexels";
-const query = `${process.env.QUERY}`;
+
 import pexelsJson from "@/data/photos.json";
-interface Photo {
-  src: {
-    large2x: string;
-  };
-  photographer: string;
-}
 
 export default function PostBox() {
-  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [photos, setPhotos] = useState([]);
   const scrollRef = useRef(null);
   // console.log(fetchPhotos("god"));
   const data = Array(pexelsJson.photos.length)
     .fill(1)
     .map((i, e) => e + 1);
   useScrollSnap({ ref: scrollRef, duration: 200 });
-
-  useEffect(() => {
-    const fetchedPhotos = fetchPhotos("god");
-    setPhotos();
-  }, []);
 
   return (
     <div className="flex justify-center w-screen">
