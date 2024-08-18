a12e149bdfe4b1d3b6e250af5175305a7343752e
Enkhbold470 <53986637+enkhbold470@users.noreply.github.com>
2024-08-18 11:50:30 -0700

    NEXT_PUBLIC is important, required for app directory

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 import { useState, useEffect } from "react";
-import { getCuratedPhotos, fetchPhotos } from "@/lib/pexels";
-import pexelsJson from "@/data/photos.json";
+import { fetchPhotos } from "@/lib/pexels";
 
+const query = `${process.env.NEXT_PUBLIC_QUERY}`;
 export default function Test() {
+  console.log(query);
   const [photos, setPhotos] = useState([]);
   useEffect(() => {
     async function loadPhotos() {
-      const fetchedPhotos = await fetchPhotos("god");
+      const fetchedPhotos = await fetchPhotos(query);
       setPhotos(fetchedPhotos);
     }
     loadPhotos();
   }, []);
+
   return (
     <div>
       {photos.map((photo: any, index: number) => (
-        <img key={photo.id} src={photo.src.original} alt={photo.alt} />
+        <img key={photo.index} src={photo.src.original} alt={photo.alt} />
       ))}
     </div>
   );
