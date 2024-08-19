8d4efb5786119a4978044925155311ce926417bb
Enkhbold470 <53986637+enkhbold470@users.noreply.github.com>
2024-08-19 05:17:57 -0700

    api endpoint for stripe for invoice

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
-import { Ubuntu, Roboto } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-const ubuntu = Ubuntu({ weight: ["400"], subsets: ["latin"] });
-const roboto = Roboto({ weight: ["300"], subsets: ["latin"] });
 import { ThemeProvider } from "@/components/theme-provider";
 
 import FloatingNavbar from "@/components/example/floating-navbar-demo";
@@ -24,7 +23,7 @@ export default function RootLayout({
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="manifest" href="/favicon_io/site.webmanifest" />
-        <body className={roboto.className}>
+        <body className={`${GeistSans.className} font-medium`}>
           {/* <Navbar />
            */}
           <ThemeProvider
